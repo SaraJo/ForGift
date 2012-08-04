a965e72965c68577bd9cd8d24e0753087425352d
Rushaine <codekid24@gmail.com>
2012-08-04 19:38:11 -0400

    comment out db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var usersByFbId = {};
 
 everyauth.debug = true;
 
-mongoose.connect("mongodb://nodejitsu:562f67fa8e47cd64081d66579e4275ec@alex.mongohq.com:10064/nodejitsudb398284603420");
+//mongoose.connect("mongodb://nodejitsu:562f67fa8e47cd64081d66579e4275ec@alex.mongohq.com:10064/nodejitsudb398284603420");
 
 
 var app = module.exports = express.createServer(
