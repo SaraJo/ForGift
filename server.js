a03226a682cfc445614e5c84232907fca443fa23
sarajo <sarajchipps@Saras-MacBook-Pro.local>
2012-08-04 16:43:05 -0400

    added database

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express'),
     connect = require('connect'),
+    mongoose = require('mongoose'),
     everyauth = require('everyauth'),
     config = require('./config');
 
 var  pub = __dirname + '/public';
 
 everyauth.debug = true;
+
+mongoose.connect("mongodb://nodejitsu:562f67fa8e47cd64081d66579e4275ec@alex.mongohq.com:10064/nodejitsudb398284603420");
+
 
 var app =express.createServer();// Configuration
 
