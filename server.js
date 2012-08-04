1d63cbf7b3e37accbbec2388a4b0ee4972bef6bc
Rushaine <codekid24@gmail.com>
2012-08-04 16:58:51 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 var express = require('express'),
     connect = require('connect'),
     everyauth = require('everyauth'),
-    config = require('./config');
+    config = require('./config'),
+    mongose = require("mongoose"),
+    Schema = mongoose.Schema,
+    http = require('http');
 
 var  pub = __dirname + '/public';
 
 everyauth.debug = true;
 
-var app =express.createServer();// Configuration
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.set('view options', {layout: false});
-app.use(require('connect').bodyParser());
-app.use(app.router);
-app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
-app.use(express.static(pub));
 
 
+var app = module.exports = express.createServer(
+      express.bodyParser(),
+      express.static(__dirname + "/public"),
+      express.cookieParser(),
+      express.session({sercet: 'teamlevo'})
+);// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+    app.use(require('connect').bodyParser());
+    app.use(app.router);
+    app.use(express.static(pub));
+    app.set('views', __dirname + '/views');
+    app.use(express.cookieParser);
+    app.use(everyauth.middleware());
+});
+
+everyauth.helpExpress(app);
 app.get('/', function(req, res){
   res.render('index');
 });
