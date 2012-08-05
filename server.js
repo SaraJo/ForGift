c2d8f9628c3dd464fc4359cf5e8f3f0a59425060
sarajo <sarajchipps@Saras-MacBook-Pro.local>
2012-08-04 23:48:34 -0400

    added photo upload

--- server.js
+++ server.js
@@ -2,15 +2,38 @@ var express = require('express'),
     connect = require('connect'),
     mongoose = require('mongoose'),
     everyauth = require('everyauth'),
-    io = require('socket.io');
+    fs = require("fs"),
+    cloudfiles = require("cloudfiles"),
+    socket = require('socket.io'),
     Schema = mongoose.Schema,
-    http = require('http');
+    http = require('http'),
+    rest = require('restler');
+
 
 var  pub = __dirname + '/public';
 
+var portInUse, cloudConf, cloud,
+    app = express.createServer(),
+    io = socket.listen( app );
+
+
+if ( process.env.NODE_ENV ) {
+  cloudConf = JSON.parse( fs.readFileSync( __dirname + "/.config", "utf-8" ) );
+
+  cloud = cloudfiles.createClient({
+    auth: {
+      username: cloudConf.username,
+      apiKey: cloudConf.apiKey
+    }
+  });
+
+  cloud.setAuth(function() {
+    console.log( "Authorized Cloud Files" );
+   });
+}
+    
 everyauth.debug = true;
 mongoose.connect("mongodb://nodejitsu:562f67fa8e47cd64081d66579e4275ec@alex.mongohq.com:10064/nodejitsudb398284603420");
-var app = express.createServer();// Configuration
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('view options', {layout: true});
@@ -19,6 +42,100 @@ app.use(app.router);
 app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
 app.use(express.static(pub));
 
+var connections = {
+};
+
+io.set( "log level", 1 );io.sockets.on( "connection", function( client ) {
+
+  client.on( "init", function( data ) {
+
+    connections[ data.id ] = true;
+
+    io.sockets.emit( "initialized", data );
+  });
+
+  client.on( "capture", function( data ) {
+
+    var file, buffer,
+        filename = data.id + "-" + Date.now() + ".png",
+        filepath = "public/saved/" + filename;
+
+    buffer = new Buffer( data.captured.replace(/^data:image\/\w+;base64,/, ""), "base64" );
+
+    file = fs.openSync( filepath, "w+" );
+
+    fs.write( file, buffer, 0, buffer.length, 0, function( err, data ) {
+      if ( err == null ) {
+
+        if ( cloud ) {
+          cloud.addFile( cloudConf.container, { remote: filename, local: filepath }, function( err, uploaded ) {
+            if ( uploaded ) {
+              console.log( "Uploaded to Cloud Files: " + filename  );
+              streamToClient([ filename ]);
+            }
+          });
+        } else {
+          rest.post('https://snapi.sincerely.com/shiplib/upload', {
+              data: {appkey:"NJ4JVDDPXKGO1K0W5L2MDO3SXK3Q5K4MJY75OQRE", photo: file},
+          }).on('complete', function(data, response) {
+              if (response.statusCode == 200) {
+               console.log(data.id, data.error);
+              }
+              else{
+                console.log(response, data.error);
+              }
+            });
+          streamToClient([ filename ]);
+        }
+
+      }
+    });
+  });
+
+  client.on( "list:request", streamList );
+
+  function streamList( data ) {
+    var id = data.id,
+        filepath = "public/saved/";
+
+    if ( cloud ) {
+      cloud.getContainer( cloudConf.container, function( err, container ) {
+        container.getFiles(function( err, files ) {
+          streamFilter( id, files.map(function( data ) { return data.name; }) );
+        });
+      });
+    } else {
+      fs.readdir( filepath, function( err, files ) {
+        streamFilter( id, files );
+      });
+    }
+  }
+
+  function streamFilter( id, files ) {
+    var size,
+        list = [];
+
+    files = files.filter(function( file ) { return (new RegExp("^" + id )).test( file ); });
+    size = files.length;
+
+    files.forEach(function( file, index ) {
+      list.push( file );
+
+      if ( index % 5 === 0 || index === size - 1 ) {
+        streamToClient( list );
+
+        list = [];
+      }
+    });
+  }
+
+  function streamToClient( list ) {
+    io.sockets.emit( "list:response", {
+      path: cloud ? "http://c309459.r59.cf1.rackcdn.com/" : "/saved/",
+      files: list
+    });
+  }
+});
 
 app.get('/', function(req, res){
   res.render('index');
