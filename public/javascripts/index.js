550e6dc9411a069cb1643b9d741330e9c001b735
Rushaine <codekid24@gmail.com>
2012-08-05 13:17:55 -0400

    index updates

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,9 @@
 $(document).ready(function (){
-    $(".friends > img").click(function(){
-      $(this).css("background-color:#E9E9E9");
+  $(".friends").hover(function(){
+    $(this).css("background-color:#E9E9E9");
+  });
+
+  $(".friends").click(function(){
+      $(this).css("background-color:#1874CD");
     });
-};
+});
